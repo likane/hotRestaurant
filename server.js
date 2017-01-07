b4b084dee32d7926aec0217eb66222b15ce3882c
Louis Kane <louiskane@1234.local>
2017-01-07 13:21:18 -0800

    added sendfile paths for each line in table

--- server.js
+++ server.js
@@ -1,34 +1,66 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//RESERVATION DATA
-//============================================================
-var reservations = [{
-	routeName: "francis",
-	phone: "9492939767",
-	email: "flao@ucla.edu",
-	uniqueID: 1
-}]
+// Table Reservations (DATA)
+// =============================================================
+var reservations = [
+
+];
+
+var tables_r = [];
+var waitlist_r = [];
 
 
-//============================================================
-app.get("/", function(req, res){
-	res.sendFile(path.join(__dirname, "index.html"))
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
 
-app.get("/reserve", function(req, res){
-	res.sendFile(path.join(__dirname, "reserve.html"))
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// Get JSON
+app.get("/api/tables", function(req, res) {
+
+
+
+    res.json(tables_r);
+
+});
+
+app.get("/api/waitlist", function(req, res) {
+
+
+
+    res.json(waitlist_r);
+
+});
+
+app.get("/api/reservations", function(req, res) {
+
+
+
+    res.json(reservations);
+
 });
 
 // Search for Specific Character (or all characters) - provides JSON
@@ -53,17 +85,24 @@ app.get("/api/:characters?", function(req, res) {
 });
 
 //CREATE NEW RESERVATIONS
-app.post("/api/reserve", function(req, res){
+app.post("/api/reservations/", function(req, res) {
 
-	//console.log("hi");
+      var newreservation = req.body;
+      newreservation.routeName = newreservation.reserve_name.replace(/\s+/g, "").toLowerCase();
 
-	var newResrvation = req.body;
-	//newResrvation.routeName = newResrvation.name.replace(/\s+/g, "").toLowerCase();
+      console.log(newreservation);
+      reservations.push(newreservation);
 
-	console.log(newResrvation);
-	reservations.push(newResrvation);
-	res.json(newResrvation);
-});
+      if (reservations.length <= 5) {
+        tables_r.push(newreservation);
+        res.json(newreservation);
+      } else {
+        waitlist_r.push(newreservation);
+        res.json(newreservation);
+      }
+  });
+
+
 
 
 
