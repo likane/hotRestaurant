1fa7d1994891c64c34bdce50c9dbb961130afcf6
Louis Kane <louiskane@1234.local>
2017-01-07 11:11:00 -0800

    server listening on PORT on NODE

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
 //============================================================
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
