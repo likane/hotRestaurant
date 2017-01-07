a49caf246ec0db063502ed3e46659dbb1dea0736
Louis Kane <louiskane@1234.local>
2017-01-07 11:34:32 -0800

    added server endpoints

--- server.js
+++ server.js
@@ -11,8 +11,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//RESERVATION DATA
+//============================================================
+var reservations = [{
+	routeName: "francis",
+	phone: "9492939767",
+	email: "flao@ucla.edu",
+	uniqueID: 1
+}]
+
 
 //============================================================
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname, "index.html"))
+});
+
+
+app.get("/reserve", function(req, res){
+	res.sendFile(path.join(__dirname, "reserve.html"))
+});
+
+//CREATE NEW RESERVATIONS
+app.post("/api/reserve", function(req, res){
+
+	var newResrvation = req.body;
+	newResrvation.routeName = newResrvation.name.replace(/\s+/g, "").toLowerCase();
+
+	console.log(newResrvation);
+	reservations.push(newResrvation);
+	res.json(newResrvation);
+});
 
 
 
